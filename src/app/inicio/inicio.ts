2c9e6c8b075a2324221d813e3ac8f64c926c8a43
valen-tiina <vale.rodriguez.palacios@gmail.com>
2025-09-03 11:22:33 -0500

    footer y pg de inicio completa

--- src/app/inicio/inicio.ts
+++ src/app/inicio/inicio.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../shared/component/header/header';
 import { Footer } from '../shared/component/footer/footer';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [Header,Footer],
+  imports: [Header,Footer,RouterModule],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css'
 })

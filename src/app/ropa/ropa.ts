abd1692777a58b2897d0f85660c662930faffa15
valen-tiina <vale.rodriguez.palacios@gmail.com>
2025-09-04 09:20:25 -0500

    detalles cambiados listas completas

--- src/app/ropa/ropa.ts
+++ src/app/ropa/ropa.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../shared/component/header/header';
 import { Footer } from '../shared/component/footer/footer';
+import { RopaList } from '../shared/component/ropa-list/ropa-list';
 
 @Component({
   selector: 'app-ropa',
-  imports: [Header,Footer],
+  imports: [Header,Footer,RopaList],
   templateUrl: './ropa.html',
   styleUrl: './ropa.css'
 })

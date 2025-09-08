539904061a5f39e5d72e2591de3a3c5c6d94d532
valen-tiina <vale.rodriguez.palacios@gmail.com>
2025-09-07 21:40:14 -0500

    final p-1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,11 @@ import { Inicio } from './inicio/inicio';
 import { Accesorios } from './accesorios/accesorios';
 import { Bolsos } from './bolsos/bolsos';
 import { Ropa } from './ropa/ropa';
+import { Contact } from './contact/contact';
 export const routes: Routes = [
     {path: '', component: Inicio},
     {path: 'accesorios', component: Accesorios},
     {path: 'bolsos', component: Bolsos},
-    {path: 'ropa', component:Ropa}
+    {path: 'ropa', component:Ropa},
+    {path: 'contact',component:Contact}
 ];

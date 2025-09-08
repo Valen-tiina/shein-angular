3584c9d24122dbfc91d83b63f9526f4e40692eb0
valen-tiina <vale.rodriguez.palacios@gmail.com>
2025-09-07 21:53:52 -0500

    final p-3

--- src/app/accesorios/accesorios.ts
+++ src/app/accesorios/accesorios.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../shared/component/header/header';
 import { Footer } from '../shared/component/footer/footer';
-import { RouterLink } from '@angular/router';
 import { AccesoriosList } from '../shared/component/accesorios-list/accesorios-list';
 
 @Component({
   selector: 'app-accesorios',
-  imports: [Header, Footer, RouterLink,AccesoriosList],
+  imports: [Header, Footer,AccesoriosList],
   templateUrl: './accesorios.html',
   styleUrl: './accesorios.css'
 })
--- src/app/bolsos/bolsos.ts
+++ src/app/bolsos/bolsos.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from '../shared/component/header/header';
 import { Footer } from '../shared/component/footer/footer';
-import { RouterLink } from '@angular/router';
+
 import { BolsosList } from '../shared/component/bolsos-list/bolsos-list';
 
 @Component({
   selector: 'app-bolsos',
-  imports: [Header,Footer,RouterLink,BolsosList],
+  imports: [Header,Footer,BolsosList],
   templateUrl: './bolsos.html',
   styleUrl: './bolsos.css'
 })
